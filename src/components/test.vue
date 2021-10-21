c2de89071da06ce6375935b3243174b6defbefd6
kimisuha <linhpham456123@gmail.com>
2021-10-21 14:46:20 +0700

    test

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,13 +1,34 @@
 <template>
-    <div>
-        <p>test</p>
+  <form action="#">
+    <div class="mb-3">
+        <label for="name" class="form-label">name</label>
+        <input type="text" class="form-control" name="name" v-model="name">
     </div>
+    <div class="mb-3">
+        <label for="sdt" class="form-label" >sdt</label>
+        <input type="tel" name="sdt"  class="form-control" v-model="sdt"/>
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">email</label>
+        <input type="email" name="email"  class="form-control" v-model="email"/>
+    </div>
+    <div class="mb-3">
+        <label for="address" class="form-label">addres</label>
+        <input type="text" name="address"  class="form-control" v-model="address"/>
+    </div>
+    <button type="button" class="btn btn-success">accesss</button>
+  </form>
 </template>
 <script>
+
 export default {
-    
-}
+    name: "Test",
+    data() {
+        sdt,
+        email,
+        address
+    }
+};
 </script>
 <style lang="scss">
-    
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Test />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Test from '../components/test.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Test
   }
 }
 </script>
